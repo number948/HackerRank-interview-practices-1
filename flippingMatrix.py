4b3842d83352dd26f501e53915c17b8806414a33
Ashley Caroca <scottsykotom@gmail.com>
2023-06-06 12:41:48 -0400

    "confihurando flipping matrix "

--- flippingMatrix.py
+++ flippingMatrix.py
@@ -1,27 +1,24 @@
 def flippingMatrix(matrix):
-  #hay que invertir la fila
+  print(matrix)
   diagonal = 0
   suma = 0
   matrix[2].reverse()
 
-  fila_temporal = matrix.pop(0)
+  '''fila_temporal = matrix.pop(0)
+  
   fila_temporal_1 = matrix.pop(2)
   fila_temporal_2 = matrix.pop(1)
 
   matrix.insert(3, fila_temporal)
   matrix.insert(0, fila_temporal_1)
-  matrix.insert(1, fila_temporal_2)
-
-
-
+  matrix.insert(1, fila_temporal_2)'''
   for i in range(len(matrix)):
         for j in range(len(matrix)):
              if i == j:
-                diagonal+=matrix[i][j]
+                diagonal+= matrix[i][j]
         
-  
   ultima = matrix[3]
-  print(matrix)
+  
   for i in ultima:
      suma+= i
   
